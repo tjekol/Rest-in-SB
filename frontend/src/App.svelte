4dbe9c33176e208b466f5f9298d19affcca75db4
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2024-09-12 11:40:26 +0200

    Welcome page, user handling, frontend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,3 +1,25 @@
-<h1>
-  Hello World
-</h1>
+<script>
+  import user from "./store/user.js";
+  import Login from "./lib/Login.svelte";
+
+  $: isLoggedIn = $user === null ? false : true;
+
+  const logout = ()  => {
+    user.update(val => val = null);
+  }
+</script>
+
+<div class="content">
+  <h1>
+    Welcome to Smart Voting App!👋🏽
+  </h1>
+  <div>
+  {#if isLoggedIn}
+    <h2>Hello, {$user.username}</h2>
+    <input type="button" value="Logout" on:click={logout}>
+  {:else}
+    <h2>Please log in to create a Poll!</h2>
+    <Login/>
+  {/if}
+  </div>
+</div>
